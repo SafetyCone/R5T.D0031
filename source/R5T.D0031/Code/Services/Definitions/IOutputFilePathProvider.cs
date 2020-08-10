8329102257499d4157ca501b3e94a55c5c24f562
David Coats <david@getsafetycone.com>
2020-08-09 23:34:26 -0700

    Added IOutputFilePathProvider service definition interface type.

---
+++ source/R5T.D0031/Code/Services/Definitions/IOutputFilePathProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0031
+{
+    public interface IOutputFilePathProvider
+    {
+        Task<string> GetOutputFilePathAsync();
+    }
+}
