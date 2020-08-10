ac48b78ec33b8ac9371acbb943aab07c619a575e
David Coats <david@getsafetycone.com>
2020-08-09 23:36:43 -0700

    Added the R5T.D0031.Default library. Added StaticValuedOutputFilePathProvider service implementation class type.

---
+++ source/R5T.D0031.Default/Code/Services/StaticValuedOutputFilePathProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0031.Default
+{
+    public class StaticValuedOutputFilePathProvider : IOutputFilePathProvider
+    {
+        public static string OutputFilePath { get; set; }
+
+
+        public Task<string> GetOutputFilePathAsync()
+        {
+            return Task.FromResult(StaticValuedOutputFilePathProvider.OutputFilePath);
+        }
+    }
+}
